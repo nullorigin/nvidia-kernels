e673bace4784bd4262c61687cfae3b2a777d6de2
Hagar Hemdan <hagarhem@amazon.com>
2024-06-04 13:05:27 +0000

    io_uring: fix possible deadlock in io_register_iowq_max_workers()
    BugLink: https://bugs.launchpad.net/bugs/2089541
    
    commit 73254a297c2dd094abec7c9efee32455ae875bdf upstream.
    
    The io_register_iowq_max_workers() function calls io_put_sq_data(),
    which acquires the sqd->lock without releasing the uring_lock.
    Similar to the commit 009ad9f0c6ee ("io_uring: drop ctx->uring_lock
    before acquiring sqd->lock"), this can lead to a potential deadlock
    situation.
    
    To resolve this issue, the uring_lock is released before calling
    io_put_sq_data(), and then it is re-acquired after the function call.
    
    This change ensures that the locks are acquired in the correct
    order, preventing the possibility of a deadlock.
    
    Suggested-by: Maximilian Heyne <mheyne@amazon.de>
    Signed-off-by: Hagar Hemdan <hagarhem@amazon.com>
    Link: https://lore.kernel.org/r/20240604130527.3597-1-hagarhem@amazon.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    CVE-2024-41080
    Signed-off-by: Manuel Diewald <manuel.diewald@canonical.com>
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- io_uring/io_uring.c
+++ io_uring/io_uring.c
@@ -10868,8 +10868,10 @@ static int io_register_iowq_max_workers(struct io_ring_ctx *ctx,
 	}
 
 	if (sqd) {
+		mutex_unlock(&ctx->uring_lock);
 		mutex_unlock(&sqd->lock);
 		io_put_sq_data(sqd);
+		mutex_lock(&ctx->uring_lock);
 	}
 
 	if (copy_to_user(arg, new_count, sizeof(new_count)))
@@ -10894,8 +10896,11 @@ static int io_register_iowq_max_workers(struct io_ring_ctx *ctx,
 	return 0;
 err:
 	if (sqd) {
+		mutex_unlock(&ctx->uring_lock);
 		mutex_unlock(&sqd->lock);
 		io_put_sq_data(sqd);
+		mutex_lock(&ctx->uring_lock);
+
 	}
 	return ret;
 }
