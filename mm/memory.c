4a656f2494305b7747c020d0b58f2fa445c14097
Timo Aaltonen <timo.aaltonen@canonical.com>
2024-06-13 15:26:22 +0300

    UBUNTU: SAUCE: Revert "mm: remove follow_pfn"
    This reverts commit cb10c28ac82c9b7a5e9b3b1dc7157036c20c36dd.
    
    Bring back follow_pfn, should allow building nvidia modules again.
    
    Modified to build after this commit:
    
    commit 29ae7d96d166fa08c7232daf8a314ef5ba1efd20
    Author: David Hildenbrand <david@redhat.com>
    Date:   Wed Apr 10 17:55:26 2024 +0200
    
        mm: pass VMA instead of MM to follow_pte()
    
    Signed-off-by: Timo Aaltonen <timo.aaltonen@canonical.com>

--- mm/memory.c
+++ mm/memory.c
@@ -6525,6 +6525,8 @@ static inline void pfnmap_lockdep_assert(struct vm_area_struct *vma)
  * before the end() is invoked.
  *
  * This function must not be used to modify PTE content.
+ * KVM uses this function.  While it is arguably less bad than ``follow_pfn``,
+ * it is not a good general-purpose API.
  *
  * Return: zero on success, negative otherwise.
  */
@@ -6619,6 +6621,35 @@ void follow_pfnmap_end(struct follow_pfnmap_args *args)
 		pte_unmap(args->ptep);
 }
 EXPORT_SYMBOL_GPL(follow_pfnmap_end);
+
+/**
+ * follow_pfn - look up PFN at a user virtual address
+ * @vma: memory mapping
+ * @address: user virtual address
+ * @pfn: location to store found PFN
+ *
+ * Only IO mappings and raw PFN mappings are allowed.
+ *
+ * This function does not allow the caller to read the permissions
+ * of the PTE.  Do not use it.
+ *
+ * Return: zero and the pfn at @pfn on success, -ve otherwise.
+ */
+int follow_pfn(struct vm_area_struct *vma, unsigned long address,
+	unsigned long *pfn)
+{
+	int ret = -EINVAL;
+	spinlock_t *ptl;
+	pte_t *ptep;
+
+	ret = follow_pte(vma, address, &ptep, &ptl);
+	if (ret)
+		return ret;
+	*pfn = pte_pfn(ptep_get(ptep));
+	pte_unmap_unlock(ptep, ptl);
+	return 0;
+}
+EXPORT_SYMBOL(follow_pfn);
 
 #ifdef CONFIG_HAVE_IOREMAP_PROT
 /**
