0c4641a124bdd1fca1d337d758f781c0f26442d0
Rodrigo Vivi <rodrigo.vivi@intel.com>
2024-08-21 15:38:31 -0400

    drm/xe: Removed unused xe_ggtt_printk
    BugLink: https://bugs.launchpad.net/bugs/2076209
    
    Apparently this was only useful when enabling ggtt support
    for the very first time and never used again.
    It is also not useful now that we have the ggtt_dump available
    through debugfs.
    
    Reviewed-by: Himal Prasad Ghimiray <himal.prasad.ghimiray@intel.com>
    Reviewed-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240821193842.352557-1-rodrigo.vivi@intel.com
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    (cherry picked from commit 69f0925c67c21c1e3ccda152d2b6ce21c363e563 linux-next)
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Signed-off-by: Ayaz A Siddiqui <ayaz.siddiqui@intel.com>
    Signed-off-by: Timo Aaltonen <timo.aaltonen@canonical.com>

--- drivers/gpu/drm/xe/xe_ggtt.h
+++ drivers/gpu/drm/xe/xe_ggtt.h
@@ -13,11 +13,8 @@ struct drm_printer;
 int xe_ggtt_init_early(struct xe_ggtt *ggtt);
 int xe_ggtt_init(struct xe_ggtt *ggtt);
 
-struct xe_ggtt_node *xe_ggtt_node_init(struct xe_ggtt *ggtt);
-void xe_ggtt_node_fini(struct xe_ggtt_node *node);
-int xe_ggtt_node_insert_balloon(struct xe_ggtt_node *node,
-				u64 start, u64 size);
-void xe_ggtt_node_remove_balloon(struct xe_ggtt_node *node);
+int xe_ggtt_balloon(struct xe_ggtt *ggtt, u64 start, u64 size, struct drm_mm_node *node);
+void xe_ggtt_deballoon(struct xe_ggtt *ggtt, struct drm_mm_node *node);
 
 int xe_ggtt_node_insert(struct xe_ggtt_node *node, u32 size, u32 align);
 int xe_ggtt_node_insert_locked(struct xe_ggtt_node *node,
