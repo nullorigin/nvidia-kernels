c81a8de497a894b072b986526aa5a784ba9229b9
Alexander Stein <alexander.stein@ew.tq-group.com>
2024-11-20 19:09:26 +0100

    i2c: lpi2c: Avoid calling clk_get_rate during transfer
    Instead of repeatedly calling clk_get_rate for each transfer, lock
    the clock rate and cache the value.
    A deadlock has been observed while adding tlv320aic32x4 audio codec to
    the system. When this clock provider adds its clock, the clk mutex is
    locked already, it needs to access i2c, which in return needs the mutex
    for clk_get_rate as well.
    
    Signed-off-by: Alexander Stein <alexander.stein@ew.tq-group.com>
    Reviewed-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Reviewed-by: Andi Shyti <andi.shyti@kernel.org>
    Signed-off-by: Andi Shyti <andi.shyti@kernel.org>
    (backported from commit 4268254a39484fc11ba991ae148bacbe75d9cc0a)
    [mpellizzer: backported using lpi2c_imx->clk instead of lpi2c_imx->clks[0].clk,
    since the double clock in lpi2c was added by 09d027dbe1713 and does not
    affect the patch]
    CVE-2024-40965
    Signed-off-by: Massimiliano Pellizzer <massimiliano.pellizzer@canonical.com>
    Acked-by: Yuxuan Luo <yuxuan.luo@canonical.com>
    Acked-by: Magali Lemes <magali.lemes@canonical.com>
    Signed-off-by: Roxana Nicolescu <roxana.nicolescu@canonical.com>

--- drivers/i2c/busses/i2c-imx-lpi2c.c
+++ drivers/i2c/busses/i2c-imx-lpi2c.c
@@ -99,6 +99,7 @@ struct lpi2c_imx_struct {
 	__u8			*rx_buf;
 	__u8			*tx_buf;
 	struct completion	complete;
+	unsigned long		rate_per;
 	unsigned int		msglen;
 	unsigned int		delivered;
 	unsigned int		block_data;
@@ -207,7 +208,8 @@ static int lpi2c_imx_config(struct lpi2c_imx_struct *lpi2c_imx)
 
 	lpi2c_imx_set_mode(lpi2c_imx);
 
-	clk_rate = clk_get_rate(lpi2c_imx->clk);
+	clk_rate = lpi2c_imx->rate_per;
+
 	if (lpi2c_imx->mode == HS || lpi2c_imx->mode == ULTRA_FAST)
 		filt = 0;
 	else
@@ -593,6 +595,20 @@ static int lpi2c_imx_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "clk enable failed %d\n", ret);
 		return ret;
 	}
+
+	/*
+	 * Lock the parent clock rate to avoid getting parent clock upon
+	 * each transfer
+	 */
+	ret = devm_clk_rate_exclusive_get(&pdev->dev, lpi2c_imx->clk);
+	if (ret)
+		return dev_err_probe(&pdev->dev, ret,
+				     "can't lock I2C peripheral clock rate\n");
+
+	lpi2c_imx->rate_per = clk_get_rate(lpi2c_imx->clk);
+	if (!lpi2c_imx->rate_per)
+		return dev_err_probe(&pdev->dev, -EINVAL,
+				     "can't get I2C peripheral clock rate\n");
 
 	pm_runtime_set_autosuspend_delay(&pdev->dev, I2C_PM_TIMEOUT);
 	pm_runtime_use_autosuspend(&pdev->dev);
