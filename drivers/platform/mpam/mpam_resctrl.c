kernelorg-mirror/linux_kernel_git_morse_linux

arm64: mpam: Add helpers to change a tasks and cpu mpam partid/pmg values
Care must be taken when modifying the partid and pmg of a task, as
writing these values may race with the task being scheduled in, and
reading the modified values.

Add helpers to set the task properties, and the cpu default value,
and add the plumbing to the mpam driver that lets resctrl use them.

Signed-off-by: James Morse <james.morse@arm.com>

drivers/platform/mpam/mpam_resctrl.c
@@ -8,6 +8,7 @@
 #include <linux/cpu.h>
 #include <linux/cpumask.h>
 #include <linux/errno.h>
+#include <linux/limits.h>
 #include <linux/list.h>
 #include <linux/printk.h>
 #include <linux/rculist.h>
@@ -70,6 +71,63 @@ int resctrl_arch_set_cdp_enabled(enum resctrl_res_level ignored, bool enable)
 u32 resctrl_arch_get_num_closid(struct rdt_resource *ignored)
 {
 	return min((u32)mpam_partid_max + 1, (u32)RESCTRL_MAX_CLOSID);
+}
+
+void resctrl_sched_in(void)
+{
+	lockdep_assert_preemption_disabled();
+
+	mpam_thread_switch(current);
+}
+
+void resctrl_arch_set_cpu_default_closid_rmid(int cpu, u32 closid, u32 pmg)
+{
+	BUG_ON(closid > U16_MAX);
+	BUG_ON(pmg > U8_MAX);
+
+	if (!cdp_enabled) {
+		mpam_set_cpu_defaults(cpu, closid, closid, pmg, pmg);
+	} else {
+		/*
+		 * When CDP is enabled, resctrl halves the closid range and we
+		 * use odd/even partid for one closid.
+		 */
+		u32 partid_d = resctrl_get_config_index(closid, CDP_DATA);
+		u32 partid_i = resctrl_get_config_index(closid, CDP_CODE);
+
+		mpam_set_cpu_defaults(cpu, partid_d, partid_i, pmg, pmg);
+	}
+}
+
+void resctrl_arch_sync_cpu_defaults(void *info)
+{
+	struct resctrl_cpu_sync *r = info;
+
+	lockdep_assert_preemption_disabled();
+
+	if (r) {
+		resctrl_arch_set_cpu_default_closid_rmid(smp_processor_id(),
+							 r->closid, r->rmid);
+	}
+
+	resctrl_sched_in();
+}
+
+void resctrl_arch_set_closid_rmid(struct task_struct *tsk, u32 closid, u32 rmid)
+{
+
+
+	BUG_ON(closid > U16_MAX);
+	BUG_ON(rmid > U8_MAX);
+
+	if (!cdp_enabled) {
+		mpam_set_task_partid_pmg(tsk, closid, closid, rmid, rmid);
+	} else {
+		u32 partid_d = resctrl_get_config_index(closid, CDP_DATA);
+		u32 partid_i = resctrl_get_config_index(closid, CDP_CODE);
+
+		mpam_set_task_partid_pmg(tsk, partid_d, partid_i, rmid, rmid);
+	}
 }
 
 bool resctrl_arch_match_closid(struct task_struct *tsk, u32 closid)

nullorigin/nvidia-kernels

(cherry picked from commit e2951d7d49e760dc0c759775950ad9ae652cd59d https://git.kernel.org/pub/scm/linux/kernel/git/morse/linux.git/log/?h=mpam/snapshot/v6.7-rc2)
Signed-off-by: KobaK <kobak@nvidia.com>
Signed-off-by: Tushar Dave <tdave@nvidia.com>
Acked-by: Carol L. Soto <csoto@nvidia.com>
Acked-by: Matthew R. Ochs <mochs@nvidia.com>
Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>

drivers/platform/mpam/mpam_resctrl.c
@@ -8,6 +8,7 @@
 #include <linux/cpu.h>
 #include <linux/cpumask.h>
 #include <linux/errno.h>
+#include <linux/limits.h>
 #include <linux/list.h>
 #include <linux/printk.h>
 #include <linux/rculist.h>
@@ -90,6 +91,63 @@ int resctrl_arch_set_cdp_enabled(enum resctrl_res_level ignored, bool enable)
 u32 resctrl_arch_get_num_closid(struct rdt_resource *ignored)
 {
 	return min((u32)mpam_partid_max + 1, (u32)RESCTRL_MAX_CLOSID);
+}
+
+void resctrl_sched_in(struct task_struct *tsk)
+{
+	lockdep_assert_preemption_disabled();
+
+	mpam_thread_switch(tsk);
+}
+
+void resctrl_arch_set_cpu_default_closid_rmid(int cpu, u32 closid, u32 pmg)
+{
+	BUG_ON(closid > U16_MAX);
+	BUG_ON(pmg > U8_MAX);
+
+	if (!cdp_enabled) {
+		mpam_set_cpu_defaults(cpu, closid, closid, pmg, pmg);
+	} else {
+		/*
+		 * When CDP is enabled, resctrl halves the closid range and we
+		 * use odd/even partid for one closid.
+		 */
+		u32 partid_d = resctrl_get_config_index(closid, CDP_DATA);
+		u32 partid_i = resctrl_get_config_index(closid, CDP_CODE);
+
+		mpam_set_cpu_defaults(cpu, partid_d, partid_i, pmg, pmg);
+	}
+}
+
+void resctrl_arch_sync_cpu_defaults(void *info)
+{
+	struct resctrl_cpu_sync *r = info;
+
+	lockdep_assert_preemption_disabled();
+
+	if (r) {
+		resctrl_arch_set_cpu_default_closid_rmid(smp_processor_id(),
+							 r->closid, r->rmid);
+	}
+
+	resctrl_sched_in(current);
+}
+
+void resctrl_arch_set_closid_rmid(struct task_struct *tsk, u32 closid, u32 rmid)
+{
+
+
+	BUG_ON(closid > U16_MAX);
+	BUG_ON(rmid > U8_MAX);
+
+	if (!cdp_enabled) {
+		mpam_set_task_partid_pmg(tsk, closid, closid, rmid, rmid);
+	} else {
+		u32 partid_d = resctrl_get_config_index(closid, CDP_DATA);
+		u32 partid_i = resctrl_get_config_index(closid, CDP_CODE);
+
+		mpam_set_task_partid_pmg(tsk, partid_d, partid_i, rmid, rmid);
+	}
 }
 
 bool resctrl_arch_match_closid(struct task_struct *tsk, u32 closid)